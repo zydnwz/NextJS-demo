52d98d2c37b19c04627b2c9b3845db1f093f18b7
zydnwz <zaidnawaz@2018@gmail.com>
2024-05-01 17:21:07 +0530

    Making Nested Routes

---
+++ pages/aboutUs/developer.js
@@ -0,0 +1,14 @@
+// our-domain.com/aboutus/developer
+
+import React from 'react';
+
+const DeveloperPage = () => {
+  return (
+    <div>
+      <h1>About Us - Developer</h1>
+      <p>This page is created by Zaid Nawaz</p>
+    </div>
+  );
+};
+
+export default DeveloperPage;
--- pages/aboutUs/index.js
+++ pages/aboutUs/index.js
@@ -1,12 +1,16 @@
 // our-domain.com/aboutus
 
 import React from 'react';
+import Link from 'next/link';
 
 const AboutUsPage = () => {
   return (
     <div>
       <h1>About Us</h1>
       <p>Welcome to our About Us page!</p>
+      <p>
+        Check out our <Link href="/aboutus/developer">Developer Page</Link>.
+      </p>
     </div>
   );
 };
